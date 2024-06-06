b9827fff3df49ffcd73181333815bd9ac6efea63
studhascja <jockhasche@gmail.com>
2024-06-06 18:21:54 +0200

    adjusted Models

--- sqs/MovieRating.Core/Models/Movie.cs
+++ sqs/MovieRating.Core/Models/Movie.cs
@@ -2,30 +2,18 @@
 
 public class Movie
 {
-    public int Id { get; set; }
+    public required Guid Id { get; set; }
 
-    public string Title { get; set; }
+    public required string Title { get; set; }
 
-    public string Director { get; set; }
+    public required string Director { get; set; }
 
-    public double Duration { get; set; }
+    public required string Duration { get; set; }
 
-    public string Genre { get; set; }
+    public required string Genre { get; set; }
 
-    public string Description { get; set; }
+    public required string Description { get; set; }
 
     public List<Rating>? Ratings { get; set; }
-
-
-    public Movie(int id, string title, string director, double duration, string genre, string description,
-        List<Rating>? ratings)
-    {
-        Id = id;
-        Title = title;
-        Director = director;
-        Duration = duration;
-        Genre = genre;
-        Description = description;
-        Ratings = ratings;
-    }
+    
 }
