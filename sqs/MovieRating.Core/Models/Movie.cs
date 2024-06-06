9799074f0f1ec7ee79a60a9871f9f0987ef9a832
studhascja <jockhasche@gmail.com>
2024-06-06 15:30:18 +0200

    Added Rating and Movie Model

---
+++ sqs/MovieRating.Core/Models/Movie.cs
@@ -0,0 +1,31 @@
+﻿namespace MovieRating.Core.Models;
+
+public class Movie
+{
+    public int Id { get; set; }
+
+    public string Title { get; set; }
+
+    public string Director { get; set; }
+
+    public double Duration { get; set; }
+
+    public string Genre { get; set; }
+
+    public string Description { get; set; }
+
+    public List<Rating>? Ratings { get; set; }
+
+
+    public Movie(int id, string title, string director, double duration, string genre, string description,
+        List<Rating>? ratings)
+    {
+        Id = id;
+        Title = title;
+        Director = director;
+        Duration = duration;
+        Genre = genre;
+        Description = description;
+        Ratings = ratings;
+    }
+}
---
+++ sqs/MovieRating.Core/Models/Rating.cs
@@ -0,0 +1,30 @@
+﻿namespace MovieRating.Core.Models;
+
+public class Rating
+{
+    public int Id { get; set; }
+
+    public string RatingNote { get; set; }
+
+    public string Author { get; set; }
+
+    public int Evaluation
+    {
+        get { return evaluation; }
+        set
+        {
+            if (value < 1 || value > 10) throw new ArgumentOutOfRangeException("Rating only allowed 1 - 10");
+            evaluation = value;
+        }
+    }
+
+    private int evaluation;
+
+    public Rating(int evaluation, int id, string ratingNote, string author)
+    {
+        Evaluation = evaluation;
+        Id = id;
+        RatingNote = ratingNote;
+        Author = author;
+    }
+}
