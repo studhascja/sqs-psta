e6b05cf0f0985dec321647d19fcd2b54991a8b6c
studhascja <jockhasche@gmail.com>
2024-06-18 18:57:24 +0200

    Add RatingController

---
+++ sqs/MovieRating.Web/Controllers/RatingController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieRating.Core.Interfaces;
+using MovieRating.Core.Models;
+using MovieRating.Web.Models;
+
+namespace MovieRating.Web.Controllers;
+
+[ApiController]
+[Route("Rating")]
+public class RatingController : ControllerBase
+{
+    private readonly IMovieService _movieService;
+    private readonly IRatingService _ratingService;
+    
+    public RatingController(IMovieService movieService, IRatingService ratingService)
+    {
+        _movieService = movieService;
+        _ratingService = ratingService;
+    }
+    
+    [HttpPost]
+    [Route("{movieTitle}")]
+    public async Task<IActionResult> AddRating(string movieTitle, [FromForm] CreateRatingDto request)
+    {
+        var movieExists = await _movieService.DoesMovieExist(movieTitle);
+        if (!movieExists)
+        {
+            return NotFound();
+        }
+
+        var movie = await _movieService.GetMovieByTitle(movieTitle);
+        var rating = new Rating
+        {
+            Id = Guid.NewGuid(),
+            RatingNote = request.Rating,
+            Author = request.Author,
+            Evaluation = 0,
+            Movie = movie
+        };
+        await _ratingService.AddRating(rating);
+        
+        return Redirect($"/Movie?title={movieTitle}");
+    }
+}
---
+++ sqs/MovieRating.Web/Models/CreateRatingDto.cs
@@ -0,0 +1,7 @@
+﻿namespace MovieRating.Web.Models;
+
+public class CreateRatingDto
+{
+    public string Author { get; set; }
+    public string Rating { get; set; }
+}
