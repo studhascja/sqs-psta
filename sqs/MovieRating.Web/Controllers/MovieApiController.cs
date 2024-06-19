049b43d474b054a36433e5d31f1e9350ec0335da
studhascja <jockhasche@gmail.com>
2024-06-19 23:17:32 +0200

    added API-Controller

---
+++ sqs/MovieRating.Web/Controllers/MovieApiController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieRating.Core.Exceptions;
+using MovieRating.Core.Interfaces;
+
+namespace MovieRating.Web.Controllers;
+
+[Route("/api/v1/Movie")]
+[ApiController]
+public class MovieApiController : ControllerBase
+{
+    private readonly IMovieService _movieService;
+    private readonly IInfoService _infoService;
+
+    public MovieApiController(IMovieService movieService, IInfoService infoService)
+    {
+        _movieService = movieService;
+        _infoService = infoService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetMovieByName(string title)
+    {
+        if (await _movieService.DoesMovieExist(title))
+        {
+            var movie = await _movieService.GetMovieByTitle(title);
+            return Ok(movie);
+        }
+
+        try
+        {
+            var movie = await _infoService.GetMovieInfo(title);
+            await _movieService.AddMovie(movie);
+            return Ok(movie);
+        }
+        catch (NoSuchMovieException e)
+        {
+            return NotFound(title);
+        }
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> ListAll()
+    {
+        var movieList = await _movieService.ListAllMovies();
+        if (movieList.Count == 0) return NotFound(movieList);
+        return Ok(movieList);
+    }
+}
