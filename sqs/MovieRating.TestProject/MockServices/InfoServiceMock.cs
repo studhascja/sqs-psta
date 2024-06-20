ab7ea4735a4739ed39432730dbff13e042d59da2
studhascja <jockhasche@gmail.com>
2024-06-20 19:08:12 +0200

    Refactored and implemented MockServices

---
+++ sqs/MovieRating.TestProject/MockServices/InfoServiceMock.cs
@@ -0,0 +1,23 @@
+﻿using MovieRating.Core.Exceptions;
+using MovieRating.Core.Interfaces;
+using MovieRating.Core.Models;
+
+namespace MovieRating.TestProject.MockServices;
+
+public class InfoServiceMock : IInfoService
+{
+    public Task<Movie> GetMovieInfo(string title)
+    {
+        if (title == "unknown") throw new NoSuchMovieException("TestMovie is unknown");
+        return Task.FromResult(new Movie
+        {
+            Id = Guid.Empty,
+            Title = "MovieFromInfoMock",
+            Director = "Mock",
+            Duration = "129 min",
+            Genre = "Action",
+            Description = "I`m mocked",
+            Ratings = []
+        });
+    }
+}
---
+++ sqs/MovieRating.TestProject/MockServices/RatingServiceMock.cs
@@ -0,0 +1,52 @@
+﻿using MovieRating.Core.Exceptions;
+using MovieRating.Core.Interfaces;
+using MovieRating.Core.Models;
+
+namespace MovieRating.TestProject.MockServices;
+
+public class RatingServiceMock : IRatingService
+{
+    private readonly List<Movie> _movieList = new();
+
+    public Task<List<Rating>> ListAllRatings(string title)
+    {
+        var matchingMovies = new List<Movie>();
+        foreach (var movie in _movieList)
+        {
+            if (movie.Title == title) matchingMovies.Add(movie);
+        }
+
+        return Task.FromResult(GetRatings(matchingMovies, title));
+    }
+
+    public Task AddRating(Rating rating)
+    {
+        foreach (var movie in _movieList)
+        {
+            if (movie.Id == rating.Movie.Id)
+            {
+                movie.Ratings.Add(rating);
+                return Task.CompletedTask;
+            }
+        }
+
+        AddMovie(rating.Movie);
+
+        return Task.CompletedTask;
+    }
+
+    public void AddMovie(Movie movie)
+    {
+        _movieList.Add(movie);
+    }
+
+    private List<Rating> GetRatings(List<Movie> movieList, string title)
+    {
+        if (movieList.Count > 1)
+            throw new ToManyMatchingRatingsException("Got " + movieList.Count + " matching Ratings for movie " + title +
+                                                     ", but only 1 is allowed.");
+        if (movieList.Count == 0) throw new NoMatchingRatingException("No matching Rating found for movie " + title);
+
+        return movieList[0].Ratings.ToList();
+    }
+}
