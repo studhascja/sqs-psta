cfa029e78033b686dc43eae7962df23db37b7a90
studhascja <jockhasche@gmail.com>
2024-06-21 11:05:03 +0200

    Added ConfigService to test EnvironmentVariableNotSetException

---
+++ sqs/MovieRating.Core/Interfaces/IConfigService.cs
@@ -0,0 +1,7 @@
+﻿namespace MovieRating.Core.Interfaces;
+
+public interface IConfigService
+{
+    string GetApiValue(string environmentNameVariable);
+    string GetDbValue(string environmentNameVariable);
+}
---
+++ sqs/MovieRating.Infrastructure/Services/ConfigService.cs
@@ -0,0 +1,17 @@
+﻿using MovieRating.Core.Exceptions;
+using MovieRating.Core.Interfaces;
+
+namespace MovieRating.Infrastructure.Services;
+
+public class ConfigService : IConfigService
+{
+    public string GetApiValue(string environmentNameVariable)
+    {
+        return Environment.GetEnvironmentVariable(environmentNameVariable) ?? throw new EnvironmentVariableNotSetException("API-Key not set correctly");
+    }
+
+    public string GetDbValue(string environmentNameVariable)
+    {
+        return Environment.GetEnvironmentVariable(environmentNameVariable) ?? throw new EnvironmentVariableNotSetException("DB-Environment-Variables not set correctly");
+    }
+}
