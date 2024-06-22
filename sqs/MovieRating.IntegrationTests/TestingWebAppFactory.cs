f95283499962c54bdb260afda576a43fcc1db0b6
studhascja <jockhasche@gmail.com>
2024-06-23 00:16:13 +0200

    added comments to integration Test

--- sqs/MovieRating.IntegrationTests/TestingWebAppFactory.cs
+++ sqs/MovieRating.IntegrationTests/TestingWebAppFactory.cs
@@ -5,18 +5,26 @@ using MovieRating.Infrastructure.Services;
 
 namespace MovieRating.IntegrationTests;
 
+/// <summary>
+/// Class <c>TestingWebAppFactory</c> provides a custom web application factory for integration testing.
+/// </summary>
 public class TestingWebAppFactory : WebApplicationFactory<Program>
 {
     private const string EnvironmentNameApiKey = "API_KEY";
     private readonly ConfigService _configService = new();
     public TestingWebAppFactory()
     {
+        // Set environment variables for API key and placeholders for database
         Environment.SetEnvironmentVariable("DB_USER", "test");
         Environment.SetEnvironmentVariable("DB_PASSWORD", "test");
         Environment.SetEnvironmentVariable("DB_SERVER", "test");
         Environment.SetEnvironmentVariable("API_KEY", _configService.GetApiValue(EnvironmentNameApiKey));
     }
     
+    /// <summary>
+    /// Configures the web host builder for integration testing.
+    /// </summary>
+    /// <param name="builder">The web host builder instance.</param>
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
         builder.ConfigureServices(services =>
